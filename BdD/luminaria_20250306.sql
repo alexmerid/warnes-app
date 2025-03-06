6f075fa880c5260b012301f395ff2b8155ac2fba
alexmerid <alexmerid@gmail.com>
2025-03-06 17:57:27 -0400

    Datos para Luminaria

---
+++ BdD/luminaria_20250306.sql
@@ -0,0 +1,8 @@
+INSERT INTO warnes.luminaria (id,tipo,potencia) VALUES
+	 (0,'No Tiene',NULL),
+	 (101,'Sodio Alta Presión',70),
+	 (102,'Sodio Alta Presión',150),
+	 (103,'Sodio Alta Presión',250),
+	 (401,'Haluro Metálico',150),
+	 (501,'LED',NULL),
+	 (502,'LED',40);
