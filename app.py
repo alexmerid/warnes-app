449e33f1a9ae4a96186418d236309952ecb4dbd8
alexmerid <alexmerid@gmail.com>
2025-03-11 21:50:50 -0400

    App Inicial

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+from flask import render_template, request, redirect
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'warnes'
+mysql.init_app(app)
+
+
+@app.route('/')
+def inicio():
+    return render_template('sitio/index.html')
+
+
+@app.route('/luminarias')
+def luminarias():
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM luminaria")
+    luminarias = cursor.fetchall()
+    conexion.commit()
+
+    return render_template('sitio/luminarias.html', luminarias=luminarias)
+
+
+@app.route('/luminarias/guardar', methods=['POST'])
+def luminarias_guardar():
+    _id = request.form['txtId']
+    _tipo = request.form['txtTipo']
+    _potencia = request.form['txtPotencia']
+
+    sql = "INSERT INTO luminaria (id, tipo, potencia) VALUES(%s, %s, %s);"
+    datos = (_id, _tipo, _potencia)
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute(sql, datos)
+    conexion.commit()
+
+    return redirect('/luminarias')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
